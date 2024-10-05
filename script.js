07d3ba3153289b9195677f07f7af98835cf1ae34
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-06 00:39:38 +0600

    pets by category function added

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const loadAllPets = async() =>{
-    const response = await fetch(` https://openapi.programming-hero.com/api/peddy/pets`)
+    const response = await fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
     const data = await response.json();
     displayAllPets(data.pets)
 }
 
 const displayAllPets = (pets) =>{
     const petsContainer = document.getElementById('pets-container');
+    petsContainer.innerHTML = ""
     pets.forEach(pet => {
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card bg-base-100 border">
-                    <figure class="px-10 pt-10">
-                        <img src=${pet.image} alt="Shoes" class="rounded-xl" />
+                    <figure class="px-6 pt-6">
+                        <img class="w-full rounded-2xl" src=${pet.image} alt="Shoes" class="rounded-xl" />
                     </figure>
                     <div class="card-body">
                         <h2 class="card-title font-bold">${pet.pet_name}</h2>
@@ -50,4 +51,71 @@ const displayAllPets = (pets) =>{
         petsContainer.appendChild(div);
     });
 }
+
+const dogsBtn = async()=>{
+    document.getElementById('pets-container').innerHTML = "";
+    document.getElementById('loading').classList.remove('hidden');
+
+    setTimeout(function() {
+        document.getElementById('loading').classList.add('hidden')
+        displayDogs(data.data)
+    }, 2000);
+    const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/dog`);
+    const data = await res.json();
+}
+
+const displayDogs = async(dogs)=>{
+
+    const petsContainer = document.getElementById('pets-container');
+    petsContainer.innerHTML = "";
+
+    
+
+    dogs.forEach(dog => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card bg-base-100 border">
+                    <figure class="px-6 pt-6">
+                        <img class="w-full rounded-2xl" src=${dog.image} alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title font-bold">${dog.pet_name}</h2>
+                        <div class="text-description">
+                            <div class="flex items-center gap-2 text-base">
+                                <img src="images/breed.png" alt="">
+                                <p>Breed: ${dog.breed?dog.breed:"Not Found"}</p>
+                            </div>
+                            <div class="flex items-center gap-2 text-base">
+                                <img src="images/calendar.png" alt="">
+                                <p>Birth: ${dog.date_of_birth?dog.date_of_birth:"Not Found"}</p>
+                            </div>
+                            <div class="flex items-center gap-2 text-base">
+                                <img src="images/gander.png" alt="">
+                                <p>Gender: ${dog.gender}</p>
+                            </div>
+                            <div class="flex items-center gap-2 text-base">
+                                <img src="images/dollar.png" alt="">
+                                <p>Price: ${dog.price?dog.price +'$':"Not Available"}</p>
+                            </div>
+                        </div>
+                        <div class="divider"></div>
+                        <div class="card-actions items-center justify-center">
+                            <button class="btn btn-sm btn-ghost border border-gray-200 text-primary font-bold text-lg"><img
+                                    src="images/like.png" alt=""></button>
+                            <button
+                                class="btn btn-sm btn-ghost border border-gray-200 text-primary font-bold text-lg">Adopt</button>
+                            <button
+                                class="btn btn-sm btn-ghost border border-gray-200 text-primary font-bold text-lg">Details</button>
+
+                        </div>
+                    </div>
+                </div>
+        
+        `
+        petsContainer.appendChild(div);
+    
+    });
+
+}
+
 loadAllPets()
