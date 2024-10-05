9b238bad7264baa4b1414136692fe41c9f590704
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-10-06 01:08:15 +0600

    category button design fixed

--- script.js
+++ script.js
@@ -52,50 +52,50 @@ const displayAllPets = (pets) =>{
     });
 }
 
-const dogsBtn = async(categoryName)=>{
+const categoryBtn = async(categoryName)=>{
     document.getElementById('pets-container').innerHTML = "";
     document.getElementById('loading').classList.remove('hidden');
 
     setTimeout(function() {
         document.getElementById('loading').classList.add('hidden')
-        displayDogs(data.data)
+        displayByCategory(data.data)
     }, 2000);
     const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${categoryName?categoryName:'not found'}`);
     const data = await res.json();
 }
 
-const displayDogs = async(dogs)=>{
+const displayByCategory = async(pets)=>{
 
     const petsContainer = document.getElementById('pets-container');
     petsContainer.innerHTML = "";
 
     
 
-    dogs.forEach(dog => {
+    pets.forEach(e => {
         const div = document.createElement('div');
         div.innerHTML = `
         <div class="card bg-base-100 border">
                     <figure class="px-6 pt-6">
-                        <img class="w-full rounded-2xl" src=${dog.image} alt="Shoes" class="rounded-xl" />
+                        <img class="w-full rounded-2xl" src=${e.image} alt="Shoes" class="rounded-xl" />
                     </figure>
                     <div class="card-body">
-                        <h2 class="card-title font-bold">${dog.pet_name}</h2>
+                        <h2 class="card-title font-bold">${e.pet_name}</h2>
                         <div class="text-description">
                             <div class="flex items-center gap-2 text-base">
                                 <img src="images/breed.png" alt="">
-                                <p>Breed: ${dog.breed?dog.breed:"Not Found"}</p>
+                                <p>Breed: ${e.breed?e.breed:"Not Found"}</p>
                             </div>
                             <div class="flex items-center gap-2 text-base">
                                 <img src="images/calendar.png" alt="">
-                                <p>Birth: ${dog.date_of_birth?dog.date_of_birth:"Not Found"}</p>
+                                <p>Birth: ${e.date_of_birth?e.date_of_birth:"Not Found"}</p>
                             </div>
                             <div class="flex items-center gap-2 text-base">
                                 <img src="images/gander.png" alt="">
-                                <p>Gender: ${dog.gender}</p>
+                                <p>Gender: ${e.gender}</p>
                             </div>
                             <div class="flex items-center gap-2 text-base">
                                 <img src="images/dollar.png" alt="">
-                                <p>Price: ${dog.price?dog.price +'$':"Not Available"}</p>
+                                <p>Price: ${e.price?e.price +'$':"Not Available"}</p>
                             </div>
                         </div>
                         <div class="divider"></div>
@@ -130,9 +130,9 @@ const displayCategories = (category) =>{
     category.forEach(e => {
         const div = document.createElement('div');
         div.innerHTML = `
-        <div onclick="dogsBtn('${e.category}')" class="cursor-pointer flex items-center gap-2 bg-gray-100 py-4 md:px-20 lg:px-24 rounded-2xl hover:rounded-full">
-                    <img class="w-10" src=${e.category_icon} alt="">
-                    <h2>${e.category}</h2>
+        <div onclick="categoryBtn('${e.category}')" class="cursor-pointer flex items-center gap-2 bg-gray-100 py-10 md:px-20 lg:px-24 rounded-3xl hover:rounded-full justify-center font-inter">
+                    <img class="w-14" src=${e.category_icon} alt="">
+                    <h2 class="text-xl font-black">${e.category}</h2>
                 </div>
         `
         categoriesConatiner.appendChild(div)
